2c101c46f2cfdbadf20a95782f8db3cdc3df02bc
shraddhagoel <shraddhagoel24@gmail.com>
2018-03-14 15:52:48 +0530

    Done

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,13 @@
 
 import React, { Component } from 'react';
 import Dropdown from 'react-dropdown';
-
+import Test from './Test.json'
 
 
 import 'react-dropdown/style.css'
 
 const options = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"]
 const defaultOption = options[0]
-
 
 
 export default class App extends Component {
@@ -19,16 +18,20 @@ export default class App extends Component {
                 selectedOption: 'Female',
                 fullname: '',
                 email: '',
-                formErrors: {email: '', name: ''},
+                formErrors: {email: '', fullname: ''},
                 emailValid: false,
                 fullnameValid: false,
-                formValid: false
+                formValid: false,
+                country: '',
+                message: '',
+                messagevalid: false
+
 
         }
 
         this.handleOptionChange = this.handleOptionChange.bind(this);
         this.inputvalid = this.inputvalid.bind(this);
-        // this.validateField = this.validateField.bind(this);
+        this.handleSelectionChange = this.handleSelectionChange.bind(this);
 
 
     }
@@ -43,11 +46,60 @@ export default class App extends Component {
         this.setState({
             selectedOption: e.target.value
 
-          },()=>  console.log(this.state.selectedOption));
+          },()=>  console.log());
          
 
     }
+    handleSelectionChange(e)
+    {
+        
+        this.setState({
+            country: e.value
 
+        })
+    }
+    componentWillMount()
+    {
+        
+        this.setState({
+            fullname: Test.Name,
+            email: Test.Email
+
+        })
+
+    }
+    shouldComponentUpdate()
+    {
+    console.log("-------------should update---------");
+    if(this.state.fullname.length<15)
+    {   console.log("Yes");
+        return true;
+    }
+    else
+    {   console.log("No");
+    let {fullnameValid,formErrors } = this.state
+    formErrors.fullname = 'Max length is 15';
+    fullnameValid = false;
+        return false;
+    }
+    }
+
+    componentDidUpdate(prevProps, prevState)
+    {       console.log("-------------did update---------");
+        let name="Diksha";
+       if(name==this.state.fullname && this.state.messagevalid==false)
+       {
+
+        this.setState({
+            message: 'Success',
+            messagevalid: true
+
+        })
+        
+       
+       }
+
+    }
     inputvalid(e)
     {
     let name= e.target.name
@@ -66,13 +118,13 @@ export default class App extends Component {
           case 'email':
             emailValid = (/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i).test(value);
             formErrors.email = emailValid ? '' : 'Invalid Email! Please Check';
-             console.log('>>>>>>>>>>>>',emailValid);
+            //  console.log('>>>>>>>>>>>>',emailValid);
 
             break;
           case 'fullname':
           fullnameValid = (/^[a-z ,.'-]+$/i).test(value);
             formErrors.fullname = fullnameValid ? '': ' Invalid Name! Please Check';
-            console.log('>>>>>>>>>>>>',fullnameValid);
+            // console.log('>>>>>>>>>>>>',fullnameValid);
 
             break;
           default:
@@ -83,21 +135,25 @@ export default class App extends Component {
                         fullnameValid: fullnameValid,
                         formValid:this.state.emailValid && this.state.fullnameValid
                       });
-                      console.log(this.state.emailValid && this.state.fullnameValid);
+                    //   console.log(this.state.emailValid && this.state.fullnameValid);
       }
       
 
     
 
     render() {
+        console.log("----------render---------");
         return (
             <header><center className="center">
                 <h1>FORM</h1>
                 <form className="new-task">
-                    <input type="text" className="input" name="fullname" placeholder="Name" onChange= {this.inputvalid } value = {this.state.name}/><h6> {this.state.formErrors.fullname} </h6>
+                    <input type="text" className="input" name="fullname" placeholder="Name" onChange= {this.inputvalid } value = {this.state.fullname}/>
+                    <h6>{this.state.formErrors.fullname} </h6>
+                    <h3>{this.state.message}</h3>
+
                     <input type="text" className="input" name="email" placeholder="Email" onChange= {this.inputvalid } value = {this.state.email} />
                     <h6> {this.state.formErrors.email} </h6>
-                    <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+                    <Dropdown options={options} onChange={this.handleSelectionChange} value={defaultOption} placeholder="Select an option" />
                     <div className="radio">
                         <label>
                             <input type="radio" value="Female" checked={this.state.selectedOption === 'Female'} onChange={this.handleOptionChange} />
@@ -115,11 +171,21 @@ export default class App extends Component {
                  <br />   <button type="button" disabled={!this.state.formValid} >Submit</button>
 
                 </form></center>
+                <center>
+                   
 
+                    <h3>{this.state.fullname}</h3>
+                    <h3>{this.state.email}</h3>
+                    <h3>{this.state.selectedOption}</h3>
+                    <h3>{this.state.country}</h3>
+                    </center>
             </header>
                 
 
         );
     }
 
+    
 }
+
+  
